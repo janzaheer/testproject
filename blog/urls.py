287c449044865db802d6a25fcf6faff8c80282f8
janzaheer <zaheerjan0@gmail.com>
2015-06-01 17:51:21 +0500

    Fixes Url

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns(
     url(r'^user', UserList.as_view(), name="user"),
     url(r'^$', StoreUser.as_view(), name="store"),
     url(r'^getuser/$', GetUser.as_view(), name='getuser'),
-    url(r'^token/$', TokenRequest.as_view(), name='getuser'),
+    url(r'^token/$', TokenRequest.as_view(), name='token'),
 )
